3f613e46ecee034e159816ab50f79710e9bb135c
vladmv96 <vladikas1996@gmail.com>
2018-10-16 21:52:51 +0300

    Updates

---
+++ precache-manifest.6b2cca2250e268f8b2e3886bdafbe89e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c4ef39806ef76eaeced814b8a5705c",
+    "url": "/funbox/static/media/Trebuchet MS.48c4ef39.ttf"
+  },
+  {
+    "revision": "6bfe3d82f4a7f7ba6ed31de38a57468f",
+    "url": "/funbox/static/media/Pattern.6bfe3d82.png"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/funbox/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "90214c75f4f723602e24bc7159d9c4c2",
+    "url": "/funbox/static/media/Cat-photo.90214c75.png"
+  },
+  {
+    "revision": "be8b6e8627b36d0925a5",
+    "url": "/funbox/static/js/runtime~main.be8b6e86.js"
+  },
+  {
+    "revision": "4127df746802a7158bca",
+    "url": "/funbox/static/js/main.4127df74.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/funbox/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "4127df746802a7158bca",
+    "url": "/funbox/static/css/main.38f4f3eb.chunk.css"
+  },
+  {
+    "revision": "9357801070b504d868b133d06a1f4c7b",
+    "url": "/funbox/index.html"
+  }
+];
